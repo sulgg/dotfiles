5f74d281a0629ae2ff64c49ba40e5b00276a8ced
Saul Guillermo <code@saul.gg>
2023-07-11 21:06:42 -0400

    more changes

--- init.lua
+++ init.lua
@@ -83,15 +83,7 @@ require("lazy").setup(
     {
         'windwp/nvim-autopairs',
         event = "InsertEnter",
-        opts = {
-            disable_filetype = { "TelescopePrompt" , "vim" },
-            check_ts = true,
-            ts_config = {
-                lua = {'string'},-- it will not add a pair on that treesitter node
-                javascript = {'template_string'},
-                java = false,-- don't check treesitter on java
-            }
-        }
+        opts = {}
     },
     'lewis6991/gitsigns.nvim',
     }
@@ -151,5 +143,15 @@ map('n', 'gb', ':BufferLinePick<CR>', {silent = true})
 
 -- ########################################################
 
+--nvim-autopairs: You can use treesitter to check for a pair.
+local npairs = require("nvim-autopairs")
+local Rule = require('nvim-autopairs.rule')
+npairs.setup({
+    check_ts = true,
+    ts_config = {
+        lua = {'string'},-- it will not add a pair on that treesitter node
+        javascript = {'template_string'},
+        java = false,-- don't check treesitter on java
+    }
+})
 local ts_conds = require('nvim-autopairs.ts-conds')
-
